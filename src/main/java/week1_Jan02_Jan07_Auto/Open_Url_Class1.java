bdccf769fa01a97cb1e2af55e1b66332a5ec2b04
wahsan80 <w_ahsan@hotmail.com>
2024-01-07 17:03:11 -0500

    Working till Jan 07/2023

--- src/main/java/week1_Jan02_Jan07_Auto/Open_Url_Class1.java
+++ src/main/java/week1_Jan02_Jan07_Auto/Open_Url_Class1.java
@@ -1,7 +1,9 @@
 package week1_Jan02_Jan07_Auto;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -11,22 +13,31 @@ public class Open_Url_Class1 {
 		
 		WebDriverManager.chromedriver().setup();
 		
-		/* WebDriverManager is an open-source Java library that carries out the management (i.e., download, 
-		 * setup, and maintenance) of the drivers required by Selenium WebDriver (e.g., chromedriver, 
-		 * geckodriver, msedgedriver, etc.) in a fully automated manner. In addition, WebDriverManager 
-		 * provides other relevant features, such as the capability to discover browsers installed in the local
-		 * system, building WebDriver objects (such as ChromeDriver, FirefoxDriver, EdgeDriver, etc.), and 
-		 * running browsers in Docker containers seamlessly.
-		 * setup() to setup the browser in an automation script. 
-		 */
-		
 		WebDriver driver = new ChromeDriver();
 		
-		//You are creating an instance of the WebDriver interface and casting it to ChromeDriver Class.
-		// WebDriver (Interface) extends ChromeDriver (Class)
-		
-		driver.get("https://www.google.com");
+		driver.get("https://www.rediff.com");
 		driver.manage().window().maximize();
+		/*
+		driver.findElement(By.className("signin")).click();
+		driver.findElement(By.id("login1")).sendKeys("seleniumpanda@rediffmail.com");
+		driver.findElement(By.id("password")).sendKeys("Selenium@123");
+		driver.findElement(By.className("signinbtn")).click();
+		*/
+		WebDriverManager.firefoxdriver().setup();
+		
+		WebDriver driver1 = new FirefoxDriver();
+		
+		driver1.get("https://www.google.com");
+		driver1.manage().window().maximize();
+		
+		System.out.println("Testing 1."); //User authentication is not allowing automation to move forward?
+		
+		//driver.findElement(By.className("rd_logout")).click();
+		
+		System.out.println("Testing 2.");
+		
+		driver.quit();
+		driver1.quit();
 		
 	}
 
