40e2670353a403f1e94de25893915d737efc7106
wahsan80 <w_ahsan@hotmail.com>
2024-03-25 13:30:05 -0400

    March 25/2024

---
+++ src/main/java/week8_Mar25_Mar31/Animal_Class.java
@@ -0,0 +1,24 @@
+package week8_Mar25_Mar31;
+
+public class Animal_Class {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This main method is in Animal class");	
+	}
+	
+	class Electronics { // Nested Class
+		
+		void mobilePhones() {
+			System.out.println("I use iPhone");
+		}
+	}
+
+}
+
+class Vehicles {
+
+	public void drive() {
+		System.out.println("Vehicles are driving");
+	}
+}
---
+++ src/main/java/week8_Mar25_Mar31/Parameterized_Methods.java
@@ -0,0 +1,22 @@
+package week8_Mar25_Mar31;
+
+public class Parameterized_Methods {
+
+	int age = 10;
+	String name = "Python";
+
+	public static void main(String[] args) {
+
+	}
+
+	public void testLogic(int age, String name) {
+		this.age = age;
+		this.name = name;
+
+	}
+
+	public void testLogin(int rollNumber, String StudentName, int classNumber) {
+
+	}
+
+}
