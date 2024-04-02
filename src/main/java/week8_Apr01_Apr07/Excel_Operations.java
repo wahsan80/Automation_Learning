96e8344faf14f6ed8715e2ed4d3b297de7ce1f52
wahsan80 <w_ahsan@hotmail.com>
2024-04-02 12:31:34 -0400

    April 02/2024

--- src/main/java/week8_Apr01_Apr07/Excel_Operations.java
+++ src/main/java/week8_Apr01_Apr07/Excel_Operations.java
@@ -10,14 +10,34 @@ public class Excel_Operations {
 		// Step4 - Operations that are required (we will code)
 		// Path: C:\Users\w_ahs\eclipse-workspace\Auto_Learning\src\main\java\week8_Apr01_Apr07\DataSample.xlsx
 		
-		String file_path = System.getProperty("user.dir" + "\\src\\main\\java\\week8_Apr01_Apr07\\DataSample.xlsx");
+		String path = System.getProperty("user.dir") + "\\src\\main\\java\\week8_Apr01_Apr07\\DataSample.xlsx";
 		
-		Xls_Reader xls = new Xls_Reader(file_path);
+		System.out.println(path);
+		
+		Xls_Reader xls = new Xls_Reader(path);
 		
 		// 1. to get the row count
 		
-		int row = xls.getRowCount("BrowserTest");
+		int row = xls.getRowCount("BrowserTest"); // Excel Sheet Name
 		System.out.println(row);
+		
+		//2. to get the column count
+		
+		int col = xls.getColumnCount("ShoppingTest"); // Excel Sheet Name
+		System.out.println(col);
+		
+		//3. Read data from Excel File
+		
+		String data = xls.getCellData("ShoppingTest", 2, 3); // Sheet Name, Row, Column
+		System.out.println(data);
+		
+		String data1 = xls.getCellData("ShoppingTest", "ProductName", 4); 
+										// Excel Sheet Name, Row Header, Column Number
+		System.out.println(data1);
+		
+		//4. write into excel sheet
+		
+		xls.setCellData("ShoppingTest", "ProductName", 11, "Testing");
 	}
 
 }
